17992e0fb849959f8c05a3fa3d4ee90c94fc4012
jiatern93 <jiatern93@hotmail.com>
2016-04-03 13:48:59 -0700

    learned passing by reference and this

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //nodejs setup
-console.log('bacon');
+//console.log('bacon');
 
 //first tutorial
-var person ={
+/*var person ={
 	firstName: "JT",
 	lastName: "Ng",
 	age: 20
@@ -22,14 +22,16 @@ console.log(worthless());
 var printBacon = function(){ 
 	console.log("bacon is healthy. Dont leave the doctors!");
 };
-//this is called an anoynomous function. A function with no name
-//the function is assigned to a variable without a name
 printBacon();
 
 setTimeout(printBacon, 5000);
+*/
+//this is called an anoynomous function. A function with no name
+//the function is assigned to a variable without a name
 
 //second tutorial
-function placeAnOrder(orderNumber){
+//this is about callback
+/*function placeAnOrder(orderNumber){
 	console.log("Customer order:", orderNumber);
 	cookAndDeliverFood(function(){
 		console.log("Delivered food order:", orderNumber);
@@ -45,3 +47,36 @@ placeAnOrder(2);
 placeAnOrder(3);
 placeAnOrder(4);
 placeAnOrder(5);
+*/
+//tutorial 3
+//this is about passing by reference
+/*var Bucky = {
+	favFood: "Bacon",
+	favMovie: "Chappie"
+};
+var Person = Bucky;
+Person.favFood = "salad";
+console.log(Bucky.favFood);
+
+console.log(19 == '19'); //true because == comapares value
+console.log(19 === '19'); //false because === compares type
+*/
+//tutorial 4
+var Bucky = {
+	printFirstName: function(){
+		console.log('My name is Bucky');
+		console.log(this === Bucky);
+	}
+};
+Bucky.printFirstName();
+
+function doSomethingWorthless(){
+		console.log('\nI am worthless');
+		console.log(this === global);
+}
+doSomethingWorthless();
+//the default calling context is global. T
+//tutorial 5
+
+
+
