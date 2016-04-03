4a37053d5702d75c31c2c57a3e63806f48ec59d6
jiatern93 <jiatern93@hotmail.com>
2016-04-03 13:38:58 -0700

    learned functions and callback

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
+//nodejs setup
 console.log('bacon');
+
+//first tutorial
+var person ={
+	firstName: "JT",
+	lastName: "Ng",
+	age: 20
+};
+console.log(person);
+
+function addNumber(a,b){
+	return a + b;
+}
+console.log(addNumber(7,3));
+
+function worthless(){
+
+}
+console.log(worthless());
+
+var printBacon = function(){ 
+	console.log("bacon is healthy. Dont leave the doctors!");
+};
+//this is called an anoynomous function. A function with no name
+//the function is assigned to a variable without a name
+printBacon();
+
+setTimeout(printBacon, 5000);
+
+//second tutorial
+function placeAnOrder(orderNumber){
+	console.log("Customer order:", orderNumber);
+	cookAndDeliverFood(function(){
+		console.log("Delivered food order:", orderNumber);
+	});
+}
+
+function cookAndDeliverFood(callback){ //simluate a 5 second operation
+	setTimeout(callback, 5000);
+}
+
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
+placeAnOrder(4);
+placeAnOrder(5);
