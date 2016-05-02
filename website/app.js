d4f88914e98958e6884774fa070ae0a1c54851ae
jiatern93 <jiatern93@hotmail.com>
2016-05-02 13:02:42 -0700

    setup a proper basic web server

--- website/app.js
+++ website/app.js
@@ -1,34 +1,56 @@
-var http = require('http');
-var fs = require('fs');
+//core modules don't need file paths
+var http = require('http'); //
+var fs = require('fs'); //file system module - allows me to read, write, and delete files
 
-http.createServer(function(request, response){
-	var url=request.url;
-	switch(url){
-		case '/':
-			getStaticFileContent(response, 'public/home.html','text/html');
-			break;
-		case '/about':
-			getStaticFileContent(response, 'public/about.html', 'text/html');
-			break;
-		case '/contact':
-			getStaticFileContent(response, 'public/contact.html', 'text/html');
-			break;
-		default:
-			response.writeHead(404, {'Content-Type':'text/plain'});
-			response.end('404 - Page not found');
-	}
-}).listen(9099);
-console.log('server running at http://localhost:9099');
-
-function getStaticFileContent(response, filepath, contentType){
-	fs.readFile(filepath, function(error, data){
-		if(error){
-			response.writeHead(500,{'Content-Type':'text/plain'});
-			response.end('500 - Internal Server Error.');
-		}
-		if(data){
-			response.writeHead(200, {'Content-Type':'text/html'});
-			response.end(data);
-		}
-	});
+//404 Error
+function send404Response(response){
+	response.writeHead(404, {'Content-Type':'text/plain'});
+	response.write("Error 404 - page not found :c");
+	response.end();
 }
+
+//Handle a user request
+function OnRequest(request, response){
+	if (request.method == 'GET' && request.url == '/'){
+		response.writeHead(200, {'Content-Type':'text/html'});
+		fs.createReadStream('./public/home.html').pipe(response); //for better performance
+	} else{
+		send404Response(response);
+	}
+}
+
+http.createServer(OnRequest).listen(9099);
+console.log("Server is now running at http://localhost:9099");
+
+//old code - sample webpage tutorial
+// http.createServer(function(request, response){
+// 	var url=request.url;
+// 	switch(url){
+// 		case '/':
+// 			getStaticFileContent(response, 'public/home.html','text/html');
+// 			break;
+// 		case '/about':
+// 			getStaticFileContent(response, 'public/about.html', 'text/html');
+// 			break;
+// 		case '/contact':
+// 			getStaticFileContent(response, 'public/contact.html', 'text/html');
+// 			break;
+// 		default:
+// 			response.writeHead(404, {'Content-Type':'text/plain'});
+// 			response.end('Error 404 - Page not found');
+// 	}
+// }).listen(9099);
+// console.log('server running at http://localhost:9099');
+
+// function getStaticFileContent(response, filepath, contentType){
+// 	fs.readFile(filepath, function(error, data){
+// 		if(error){
+// 			response.writeHead(500,{'Content-Type':'text/plain'});
+// 			response.end('500 - Internal Server Error.');
+// 		}
+// 		if(data){
+// 			response.writeHead(200, {'Content-Type':'text/html'});
+// 			response.end(data);
+// 		}
+// 	});
+// }
