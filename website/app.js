b33569074504b9aa820c6b2e6e3c27c714baa224
jiatern93 <jiatern93@hotmail.com>
2016-09-22 16:49:28 +0800

    hi

--- website/app.js
+++ website/app.js
@@ -1,9 +1,9 @@
 //core modules don't need file paths
 var http = require('http'); //
 var fs = require('fs'); //file system module - allows me to read, write, and delete files
-var connect = require('connect');
+var express = require('express');
 
-var app=connect();
+var app=express();
 
 function profile(request, response){
 	console.log('user requested profile');
@@ -29,36 +29,3 @@ function OnRequest(request, response){
 http.createServer(OnRequest).listen(9099);
 app.use('/profile', profile);
 console.log("Server is now running at http://localhost:9099");
-
-//old code - sample webpage tutorial
-// http.createServer(function(request, response){
-// 	var url=request.url;
-// 	switch(url){
-// 		case '/':
-// 			getStaticFileContent(response, 'public/home.html','text/html');
-// 			break;
-// 		case '/about':
-// 			getStaticFileContent(response, 'public/about.html', 'text/html');
-// 			break;
-// 		case '/contact':
-// 			getStaticFileContent(response, 'public/contact.html', 'text/html');
-// 			break;
-// 		default:
-// 			response.writeHead(404, {'Content-Type':'text/plain'});
-// 			response.end('Error 404 - Page not found');
-// 	}
-// }).listen(9099);
-// console.log('server running at http://localhost:9099');
-
-// function getStaticFileContent(response, filepath, contentType){
-// 	fs.readFile(filepath, function(error, data){
-// 		if(error){
-// 			response.writeHead(500,{'Content-Type':'text/plain'});
-// 			response.end('500 - Internal Server Error.');
-// 		}
-// 		if(data){
-// 			response.writeHead(200, {'Content-Type':'text/html'});
-// 			response.end(data);
-// 		}
-// 	});
-// }
